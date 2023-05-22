0d0e1f49c16b561bc0b2401ff30f83d9e3eae9b9
mcastiello8 <129375473+mcastiello8@users.noreply.github.com>
2023-05-22 16:38:20 +0200

    creato model

---
+++ src/main/java/it/prova/agendarest/model/Agenda.java
@@ -0,0 +1,92 @@
+package it.prova.agendarest.model;
+
+import java.time.LocalTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "agenda")
+public class Agenda {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+	
+	@Column(name = "descrizione")
+	private String descrizione;
+
+	@Column(name = "dataOraInizio")
+	private LocalTime dataOraInizio;
+
+	@Column(name = "dataOraFine")
+	private LocalTime dataOraFine;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "utente_id", nullable = false)
+	private Utente utente;
+
+	
+	public Agenda() {}
+	
+	public Agenda(Long id, String descrizione, LocalTime dataOraInizio, LocalTime dataOraFine, Utente utente) {
+		super();
+		this.id = id;
+		this.descrizione = descrizione;
+		this.dataOraInizio = dataOraInizio;
+		this.dataOraFine = dataOraFine;
+		this.utente = utente;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescrizione() {
+		return descrizione;
+	}
+
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+
+	public LocalTime getDataOraInizio() {
+		return dataOraInizio;
+	}
+
+	public void setDataOraInizio(LocalTime dataOraInizio) {
+		this.dataOraInizio = dataOraInizio;
+	}
+
+	public LocalTime getDataOraFine() {
+		return dataOraFine;
+	}
+
+	public void setDataOraFine(LocalTime dataOraFine) {
+		this.dataOraFine = dataOraFine;
+	}
+
+	public Utente getUtente() {
+		return utente;
+	}
+
+	public void setUtente(Utente utente) {
+		this.utente = utente;
+	}
+
+}
+
+
